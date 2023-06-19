e7a18c8db95f2c4af78e998f4d99473ba8dff73b
eXcea <137011583+eXcea@users.noreply.github.com>
2023-06-19 18:01:49 -0400

    Add files via upload

---
+++ core.lua
@@ -0,0 +1,9 @@
+local awful = require("awful")
+
+-- Use the 'awful' module
+awful:Print("Hello, world!")
+
+awful.DevMode = true
+
+project.warrior = {}
+project.paladin.holy = awful.Actor:New({ spec = 1, class = "holy" })
