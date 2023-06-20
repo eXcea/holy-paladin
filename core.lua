54044fd068a9c76ad5098d02467187db5fabba55
eXcea <137011583+eXcea@users.noreply.github.com>
2023-06-19 22:15:20 -0400

    Delete core.lua

--- core.lua
+++
@@ -1,9 +0,0 @@
-local awful = require("awful")
-
--- Use the 'awful' module
-awful:Print("Hello, world!")
-
-awful.DevMode = true
-
-project.warrior = {}
-project.paladin.holy = awful.Actor:New({ spec = 1, class = "holy" })
