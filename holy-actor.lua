0f4178643106830e1e7e2b73cb74d16c64a12567
eXcea <137011583+eXcea@users.noreply.github.com>
2023-06-20 01:43:56 -0400

    Update holy-actor.lua

--- holy-actor.lua
+++ holy-actor.lua
@@ -1,21 +1,12 @@
-local Unlocker, awful, project = holy
+local Unlocker, awful, project = ...
 local holy = project.paladin.holy
 
-local Spell = awful.Spell
-awful.Populate({
+print("eXcre Holy Paladin rotation loaded and ready!")
 
-  -- static objects [not req. but tiny perf. increase and takes care of declaration where i use them]
-  target = awful.target,
-  focus = awful.focus,
-  player = awful.player,
-  healer = awful.healer,
-  pet = awful.pet,
-  enemyHealer = awful.enemyHealer,
-
-  -- damage
-  holyshock = Spell(20473, { damage = "magical", targeted = true }),
-  crusaderstrike = Spell(35395, { damage = "physical", targeted = true }),
-  judgement = Spell(343122, { damage = "magucal", targeted = true }),
-}   
-end
-end)
+paladin:Init(function()
+    -- Keep in mind that we only want to execute our rotation if there is an enemy target.
+    if target.enemy then
+        StartAttack()
+        holyshock()
+        crusaderstrike()
+        judgement()
